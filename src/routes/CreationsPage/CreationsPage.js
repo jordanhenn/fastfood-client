02881bb7d5f3b748536432e957b881b5b65ad020
Jordan Henn <jordanwalkerhenn@gmail.com>
2020-12-09 22:38:09 -0800

    static client

--- src/routes/CreationsPage/CreationsPage.js
+++ src/routes/CreationsPage/CreationsPage.js
@@ -8,6 +8,7 @@ class CreationsPage extends Component {
     return (
       <section className='Creations'>
         <h2>Previous Creations</h2>
+        <p>None created yet. Check back again later, haha</p>
         <ul className='Creations-List' aria-live='polite'>
           {creations.map(creation =>
             <Creation
